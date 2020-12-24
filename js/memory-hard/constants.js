5510ac5c09886362e29af4ec695c34385f4fc2fe
cupshir <christopherupshir@ccv.church>
2020-12-23 21:58:45 -0700

    More assets for memory game

--- js/memory-hard/constants.js
+++ js/memory-hard/constants.js
@@ -30,63 +30,63 @@ const AVAILABLE_CARDS = [
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-1", 
-        "ImageName": "sailor-moon-1.png",
+        "Type": "sailor-moon-7", 
+        "ImageName": "sailor-moon-7.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-2", 
-        "ImageName": "sailor-moon-2.png",
+        "Type": "sailor-moon-8", 
+        "ImageName": "sailor-moon-8.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-3", 
-        "ImageName": "sailor-moon-3.png",
+        "Type": "sailor-moon-9", 
+        "ImageName": "sailor-moon-9.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-4", 
-        "ImageName": "sailor-moon-4.png",
+        "Type": "sailor-moon-10", 
+        "ImageName": "sailor-moon-10.png",
          "ImageText": "Sailor Moon"
     },    
     {
-        "Type": "sailor-moon-5", 
-        "ImageName": "sailor-moon-5.png",
+        "Type": "sailor-moon-11", 
+        "ImageName": "sailor-moon-11.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-6", 
-        "ImageName": "sailor-moon-6.png",
+        "Type": "sailor-moon-12", 
+        "ImageName": "sailor-moon-12.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-1", 
-        "ImageName": "sailor-moon-1.png",
+        "Type": "sailor-moon-13", 
+        "ImageName": "sailor-moon-13.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-2", 
-        "ImageName": "sailor-moon-2.png",
+        "Type": "sailor-moon-14", 
+        "ImageName": "sailor-moon-14.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-3", 
-        "ImageName": "sailor-moon-3.png",
+        "Type": "sailor-moon-15", 
+        "ImageName": "sailor-moon-15.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-4", 
-        "ImageName": "sailor-moon-4.png",
+        "Type": "sailor-moon-16", 
+        "ImageName": "sailor-moon-16.png",
          "ImageText": "Sailor Moon"
     },    
     {
-        "Type": "sailor-moon-5", 
-        "ImageName": "sailor-moon-5.png",
+        "Type": "sailor-moon-17", 
+        "ImageName": "sailor-moon-17.png",
          "ImageText": "Sailor Moon"
     },
     {
-        "Type": "sailor-moon-6", 
-        "ImageName": "sailor-moon-6.png",
+        "Type": "sailor-moon-18", 
+        "ImageName": "sailor-moon-18.png",
          "ImageText": "Sailor Moon"
     }
 ]
